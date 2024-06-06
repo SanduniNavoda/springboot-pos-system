d3379219261488bb00796f46e39aec021c8dc214
sanduni <sanduniakn@gmail.com>
2024-06-07 03:26:34 +0530

    Updated security

--- src/main/java/com/ijse/hellospring/repository/UserRepository.java
+++ src/main/java/com/ijse/hellospring/repository/UserRepository.java
@@ -1,13 +1,18 @@
 package com.ijse.hellospring.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ijse.hellospring.entity.User;
+import java.util.List;
+
 
 //same as DAO layer in layered architecture
 
 @Repository
 public interface UserRepository extends JpaRepository <User, Long>{
     //Advanced Custom Queries
+    Optional<User> findByUsername(String username);
 }
